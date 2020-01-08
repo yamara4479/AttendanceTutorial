7d6d976e87a17a22d840d2e7aead05e941f10753
yamara4479 <yamara0709@outlook.jp>
2020-01-08 10:17:30 +0000

    Finish editing-users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,21 @@
+# coding: utf-8
+
+User.create!(name: "Sample User",
+             email: "sample@email.com",
+             password: "password",
+             password_confirmation: "password",
+             admin: true)
+
+60.times do |n|
+  name  = Faker::Name.name
+  email = "sample-#{n+1}@email.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
