d08af77d24cb931331dcdb9ac28a930d9134793d
yamara4479 <yamara0709@outlook.jp>
2020-01-10 08:35:54 +0000

    Finish japanization-and-time-format

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,15 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
 module AttendanceApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    
     config.load_defaults 5.1
     config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    
   end
 end
